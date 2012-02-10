51d369d15bc9261f9174d28e9bcee2e71153bd00
heapifyman <heapifyman@gmail.com>
2012-02-10 14:58:08 +0100

    - added some jquery stuff

--- src/main/java/org/heapifyman/next/MightBeWorkingPage.java
+++ src/main/java/org/heapifyman/next/MightBeWorkingPage.java
@@ -3,9 +3,13 @@ package org.heapifyman.next;
 import org.apache.log4j.Logger;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.extensions.ajax.markup.html.IndicatingAjaxButton;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.PropertyModel;
 import org.heapifyman.HomePage;
 
@@ -24,13 +28,14 @@ public class MightBeWorkingPage extends WebPage {
 
 	private Integer counter = 0;
 
+	private String tags = "eins,zwei,drei";
+
 	public MightBeWorkingPage() {
+		
+		logger.debug("starting MightBeWorkingPage");
 
 		add(new Link<HomePage>("home") {
 
-			/**
-			 * 
-			 */
 			private static final long serialVersionUID = -8795500883865254328L;
 
 			@Override
@@ -47,9 +52,6 @@ public class MightBeWorkingPage extends WebPage {
 
 		add(new AjaxLink<Void>("ajaxLink") {
 
-			/**
-			 * 
-			 */
 			private static final long serialVersionUID = -8573529242165195949L;
 
 			@Override
@@ -59,6 +61,25 @@ public class MightBeWorkingPage extends WebPage {
 				target.add(ajaxLabel);
 			}
 
+		});
+
+		final Form<MightBeWorkingPage> form = new Form<MightBeWorkingPage>("tagForm", new CompoundPropertyModel<MightBeWorkingPage>(this));
+		add(form);
+		final TextField<String> tagsTextField = new TextField<String>("tags");
+		form.add(tagsTextField.setOutputMarkupId(true));
+		form.add(new IndicatingAjaxButton("saveButton", form) {
+
+			private static final long serialVersionUID = -3901377832995484253L;
+
+			@Override
+			protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
+				logger.debug(tags);
+			}
+
+			@Override
+			protected void onError(AjaxRequestTarget target, Form<?> form) {
+				logger.debug("D'oh");
+			}
 		});
 
 	}
@@ -76,5 +97,20 @@ public class MightBeWorkingPage extends WebPage {
 	 */
 	public void setCounter(Integer counter) {
 		this.counter = counter;
+	}
+
+	/**
+	 * @return the tags
+	 */
+	public String getTags() {
+		return tags;
+	}
+
+	/**
+	 * @param tags
+	 *            the tags to set
+	 */
+	public void setTags(String tags) {
+		this.tags = tags;
 	}
 }
